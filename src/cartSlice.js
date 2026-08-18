4a62e010f01dad2b16307e011038fbe0ac2188da
jsanchezp-max <j.sanchezp@lanuevaamerica.edu.co>
2026-08-18 00:08:29 +0000

    Agregar cartSlice.js para estado de Redux Toolkit

---
+++ src/cartSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Lista de plantas agregadas
+  },
+  reducers: {
+    // Agregar producto al carrito
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+    // Eliminar producto del carrito
+    removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+    },
+    // Incrementar cantidad
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
+
+export default cartSlice.reducer;
